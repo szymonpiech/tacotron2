bf04ce0bbc2009c75cd648c65d544e9096288491
41WhiteElephants <wacol72@gmail.com>
2020-08-02 21:02:44 +0100

    alter hparams for transfer-learning

--- hparams.py
+++ hparams.py
@@ -58,8 +58,8 @@ def create_hparams(hparams_string=None, verbose=False):
         prenet_dim=256,
         max_decoder_steps=1000,
         gate_threshold=0.5,
-        p_attention_dropout=0.1,
-        p_decoder_dropout=0.1,
+        p_attention_dropout=0.5,
+        p_decoder_dropout=0.5,
 
         # Attention parameters
         attention_rnn_dim=1024,
@@ -78,7 +78,7 @@ def create_hparams(hparams_string=None, verbose=False):
         # Optimization Hyperparameters #
         ################################
         use_saved_learning_rate=False,
-        learning_rate=1e-3,
+        learning_rate=4e-5,
         weight_decay=1e-6,
         grad_clip_thresh=1.0,
         batch_size=16,
