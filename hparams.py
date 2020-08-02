ba66ec67829cb7dc04bd1c83beb062336a4d55d6
41WhiteElephants <wacol72@gmail.com>
2020-08-02 09:45:16 +0200

    Fit iteration per epoch to dataset size

--- hparams.py
+++ hparams.py
@@ -9,8 +9,8 @@ def create_hparams(hparams_string=None, verbose=False):
         ################################
         # Experiment Parameters        #
         ################################
-        epochs=500,
-        iters_per_checkpoint=1000,
+        epochs=2000,
+        iters_per_checkpoint=228,
         seed=1234,
         dynamic_loss_scaling=True,
         fp16_run=False,
@@ -81,7 +81,7 @@ def create_hparams(hparams_string=None, verbose=False):
         learning_rate=1e-3,
         weight_decay=1e-6,
         grad_clip_thresh=1.0,
-        batch_size=64,
+        batch_size=16,
         mask_padding=True  # set model's padded outputs to padded values
     )
 
